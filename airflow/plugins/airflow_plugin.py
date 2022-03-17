4f2daa5f96c0dea153dabb59f5709e54bcd45b32
Cesar Augusto <cesar.bruschetta@olxbr.com>
2022-03-17 13:17:54 -0300

    Remove airflow

--- airflow/plugins/airflow_plugin.py
+++
@@ -1,7 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-from hooks.twitter_hook import TwitterHook
-
-
-class CustomAirflowPlugin(AirflowPlugin):  # type: ignore
-    name = "custom"
-    hooks = [TwitterHook]
