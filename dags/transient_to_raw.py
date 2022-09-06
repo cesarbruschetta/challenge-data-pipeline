73501d3fdb5cd4ca142eed057ef01b8abf05fd94
Cesar Augusto <cesarabruschetta@gmail.com>
2022-09-06 12:25:17 -0300

    Add dags

---
+++ dags/transient_to_raw.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+from airflow.models import DAG, Variable
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+
+ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(6),
+}
+BASE_FOLDER = Path(__file__).parent
+
+with DAG(
+    dag_id='transient_to_raw_v1',
+    default_args=ARGS,
+    schedule_interval='0 10 * * *',
+    max_active_runs=1,
+) as dag:
+
+    start = DummyOperator(task_id='start')
+
+    save_transient_raw = KubernetesPodOperator(
+        task_id='run_save_transient_raw',
+        name='run_save_transient_raw',
+        cmds=[
+            f'python /app/pipeline_twitter/etls/save_in_raw.py',
+            '--start_time={{ yesterday_ds }}',
+            '--end_time={{ ds }}',
+        ],
+        image_pull_policy="Always",
+        namespace='airflow',
+        image="cesarbruschetta/challenget-data-pipeline:lastest",
+        is_delete_operator_pod=True,
+        env_vars={},
+    )
+
+    end = DummyOperator(task_id='end')
+
+    start >> save_transient_raw >> end
