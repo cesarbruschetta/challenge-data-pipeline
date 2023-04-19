d168530c30ee9016b726d88457133a0c37a4c40c
Cesar Augusto <cesar.bruschetta@olxbr.com>
2023-04-19 11:37:08 -0300

    Update code submit

--- srcs/spark-submit/submit/main.py
+++ srcs/spark-submit/submit/main.py
@@ -21,26 +21,29 @@ def main():
         '/app/pipeline_twitter/etls/save_in_raw.py '
         f'--process-date={os.environ.get("AIRFLOW_PROCESS_DATE")}'
     )
-    
-    process = subprocess.Popen(
+
+    process = subprocess.run(
         [
-            f'{BASE_PATH}/submit.sh',
+            '/bin/bash', "-c",  f'{BASE_PATH}/submit.sh',
         ],
         stdout=subprocess.PIPE, 
         stderr=subprocess.PIPE,
         shell=True,
+        check=True,
         cwd=BASE_PATH,
-        env={
-            'APP_NAME': app_name,
-            'IMAGE': image,
-            'FILE_PATH': file_path,
-            'SERVICE_ACCOUNT': service_account,
-        }
+        env=dict(
+            os.environ,
+            **{
+                'APP_NAME': app_name,
+                'IMAGE': image,
+                'FILE_PATH': file_path,
+                'SERVICE_ACCOUNT': service_account,
+            }
+        )
     )
-
-    stdout, stderr = process.communicate()
-    stdout, stderr = stdout.decode('utf-8'), stderr.decode('utf-8')
-    logger.info(f'Finished spark-submit. stdout={stdout}, stderr={stderr}')
+    process.check_returncode()
+    stdout = process.stdout.decode('utf-8')
+    logger.info(f'Finished spark-submit. stdout={stdout}')
 
 
 if __name__ == "__main__":
