5c211cad8a33cb5a5db464f43aeb91101132cbfb
Cesar Augusto <cesar.bruschetta@olxbr.com>
2023-04-25 12:23:01 -0300

    Update spark-submit

--- srcs/spark-submit/submit/main.py
+++ srcs/spark-submit/submit/main.py
@@ -22,27 +22,25 @@ def main():
         f'--process-date={os.environ.get("AIRFLOW_PROCESS_DATE")}'
     )
 
-    process = subprocess.run(
-        [
-            '/bin/bash', "-c",  f'{BASE_PATH}/submit.sh',
-        ],
+    process = subprocess.Popen(
+        f'/bin/bash -c {BASE_PATH}/submit.sh',
         stdout=subprocess.PIPE, 
         stderr=subprocess.PIPE,
         shell=True,
-        check=True,
+        universal_newlines=True,
         cwd=BASE_PATH,
-        env=dict(
-            os.environ,
-            **{
-                'APP_NAME': app_name,
-                'IMAGE': image,
-                'FILE_PATH': file_path,
-                'SERVICE_ACCOUNT': service_account,
-            }
-        )
+        env=dict(os.environ, **{
+            'APP_NAME': app_name,
+            'IMAGE': image,
+            'SERVICE_ACCOUNT': service_account,
+            'FILE_PATH': file_path,
+        })
     )
-    process.check_returncode()
-    stdout = process.stdout.decode('utf-8')
+    stdout, stderr = process.communicate()
+    if process.returncode !=0:
+        logger.error(f'Failed to run spark-submit. stderr={stderr}')
+        raise SystemExit(f'Failed to run spark-submit. stderr={stderr}', code=process.returncode)
+
     logger.info(f'Finished spark-submit. stdout={stdout}')
 
 
